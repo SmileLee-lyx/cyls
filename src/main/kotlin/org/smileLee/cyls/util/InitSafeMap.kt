53581dacc0f7250b041deca7d667d356a5c67aef
SmileLee <944058846@qq.com>
2017-07-22 00:59:04 +0800

    修改工具类SafeMap，使其继承自MutableMap

--- src/main/kotlin/org/smileLee/cyls/util/InitSafeMap.kt
+++ src/main/kotlin/org/smileLee/cyls/util/InitSafeMap.kt
@@ -2,14 +2,30 @@ package org.smileLee.cyls.util
 
 class InitSafeMap<K, V>(val m: HashMap<K, V>, val init: (K) -> V) : SafeMap<K, V> {
     override fun put(key: K, value: V) = m.put(key, value)
-    override fun putAll(from: Map<K, V>) = m.putAll(from)
+    override fun putAll(from: Map<out K, V>) = m.putAll(from)
     override fun iterator() = m.iterator()
-    override fun get(key: K): V {
-        val nullableValue = m[key]
-        if (nullableValue != null) return nullableValue else {
-            val value = init(key)
-            m.put(key, value)
-            return value
-        }
+    override fun get(key: K): V = m[key] ?: let {
+        val value = init(key)
+        m.put(key, value)
+        value
     }
+
+    override val size: Int
+        get() = m.size
+
+    override fun containsKey(key: K) = m.containsKey(key)
+    override fun containsValue(value: V) = m.containsValue(value)
+    override fun isEmpty() = m.isEmpty()
+    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+        get() = m.entries
+    override val keys: MutableSet<K>
+        get() = m.keys
+    override val values: MutableCollection<V>
+        get() = m.values
+
+    override fun clear() {
+        m.clear()
+    }
+
+    override fun remove(key: K) = m.remove(key)
 }
--- src/main/kotlin/org/smileLee/cyls/util/SafeMap.kt
+++ src/main/kotlin/org/smileLee/cyls/util/SafeMap.kt
@@ -2,9 +2,9 @@ package org.smileLee.cyls.util
 
 import kotlin.collections.MutableMap.*
 
-interface SafeMap<K, V> {
-    fun put(key: K, value: V): V?
-    fun putAll(from: Map<K, V>)
-    operator fun get(key: K): V
-    operator fun iterator(): MutableIterator<MutableEntry<K, V>>
+interface SafeMap<K, V> : MutableMap<K, V>, Iterable<Map.Entry<K, V>> {
+    override fun put(key: K, value: V): V?
+    override fun putAll(from: Map<out K, V>)
+    override operator fun get(key: K): V
+    override operator fun iterator(): MutableIterator<MutableEntry<K, V>>
 }
